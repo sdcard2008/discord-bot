e79e056c859980c7e3b854b0fe741124ec513950
sdcard2008 <saptak1234hehe@gmail.com>
2022-06-09 15:08:26 +0530

    Separated the client from bot

--- bot.py
+++ bot.py
@@ -1,39 +1,28 @@
 import os
-import random
 
-import discord
+import  random
+
+from discord.ext import commands
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DISCORD_TOKEN = os.getenv("TOKEN")
+DISCORD_TOKEN = os.getenv('TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="/")
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} is now Online!')
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    print(f'{bot.user.name} has connected to discord!!')
+@bot.command(name="joke")
+async def tell_a_joke(ctx):    
     jokes = [
         "*insert joke here about school*" ,
         "*insert joke about sport*" ,
         "*insert joke about haha funny*"
         #etc etc
     ]
-    when_to_tell_joke = [
-        "Tell me a joke!" ,
-        "tell me a joke" ,
-        "I am bored" ,
-        "Tell me a joke"
 
-        #etc.
-    ]
-
-    if message.content in when_to_tell_joke:
-        await message.channel.send(random.choice(jokes))
-client.run(DISCORD_TOKEN)        
-            
+    await ctx.send(random.choice(jokes))
+bot.run(DISCORD_TOKEN)    
